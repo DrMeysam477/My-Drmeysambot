c3f39f8bd1892d50d5365c6619c39caf1c7d7ca7
DrMeysam477 <Meysamcircle@gmail.com>
2026-06-08 20:39:12 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,15 @@
 import telebot
 
-# توکن را دقیقاً بین دو علامت کوتیشن قرار دادیم
 TOKEN = '8979791105:AAGLK5usKZ54g7R40msQ2YkrN2cgUCegaLc'
 
 bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
-    bot.reply_to(message, "سلام! من در سرور رندر بیدار هستم و ۲۴ ساعته کار می‌کنم.")
+    bot.reply_to(message, "سلام! من در سرور رندر بیدار هستم.")
 
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
     bot.reply_to(message, message.text)
 
-# این خط برای روشن نگه داشتن ربات ضروری است
 bot.infinity_polling()
